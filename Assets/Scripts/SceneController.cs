79226c50d2f5c3f1b2043bd240697337c5000e8c
ryou <visualjupiter@gmail.com>
2018-04-14 22:35:41 +0900

    シーン遷移を仮実装

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+    public void ToStart()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+    public void ToStageSelect()
+    {
+        SceneManager.LoadScene("StageSelect");
+    }
+
+    public void ToStage()
+    {
+        SceneManager.LoadScene("Stage_01");
+    }
+
+    public void GameEnd()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+        // WebPlayerの際の処理を記述
+		//Application.OpenURL("http://www.yahoo.co.jp/");
+#else
+		Application.Quit();
+#endif
+    }
+}
